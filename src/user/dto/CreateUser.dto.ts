1e19e7d39b7e2707449ec24062567540294011c0
Felipe <febomtempo@hotmail.com>
2023-07-07 17:28:39 -0300

    feat: user validation

---
+++ src/user/dto/CreateUser.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, MinLength, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsNotEmpty()
+  name: string;
+  @IsEmail()
+  email: string;
+  @MinLength(6)
+  password: string;
+}
